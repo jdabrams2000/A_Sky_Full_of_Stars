0af363c0d5e70809689e0f613bb138ea525c4cfd
jdabrams2000 <abramsj@bxscience.edu>
2017-09-24 23:50:30 -0400

    Add Project #1

---
+++ Night Sky/Assets/MoveCelestial.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCelestial : MonoBehaviour {
+
+	public float speed;
+	public float initX;
+	public float initY;
+
+	public float r;
+	public float degrees = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		float r = Mathf.Sqrt(initX * initX + initY * initY);
+		transform.position = new Vector3(initX, initY, 0.0f);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		degrees = (speed * (degrees +1)) % 360.0f;
+		transform.position = new Vector3(r * Mathf.Cos(degrees), r * Mathf.Sin(degrees), 0.0f);
+	}
+}
--- Night Sky/Assets/MoveCharacter.cs
+++ Night Sky/Assets/MoveCharacter.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 
 public class MoveCharacter : MonoBehaviour {
-
 	public float speed;
 	private Rigidbody rb;
 
@@ -10,14 +9,14 @@ public class MoveCharacter : MonoBehaviour {
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
 	}
-	
+
 	// Update is called once per frame
 	void FixedUpdate () {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-
 		rb.AddForce (movement);
 	}
+
 }
