2f85230e0ad090acb1cb7c88a22d2aa7581663e9
jdabrams2000 <abramsj@bxscience.edu>
2017-09-25 23:11:05 -0400

    Add Project #1

--- Night Sky/Assets/MoveCelestial.cs
+++ Night Sky/Assets/MoveCelestial.cs
@@ -3,23 +3,10 @@ using System.Collections;
 
 public class MoveCelestial : MonoBehaviour {
 
-	public float speed;
-	public float initX;
-	public float initY;
-
-	private float r;
-	private float degrees;
-
-	// Use this for initialization
-	void Start () {
-		r = Mathf.Sqrt(initX * initX + initY * initY);
-		degrees = 0.0f;
-		transform.position = new Vector3(initX, initY, 0.0f);
-	}
+	public float speed = 50f;
 	
 	// Update is called once per frame
-	void FixedUpdate () {
-		degrees = (speed * (degrees +1)) % 360.0f;
-		transform.position = new Vector3(r * Mathf.Cos(degrees), r * Mathf.Sin(degrees), 0.0f);
+	void Update () {
+		transform.RotateAround(Vector3.zero, Vector3.back, speed * Time.deltaTime);
 	}
 }
--- Night Sky/Assets/MoveCharacter.cs
+++ Night Sky/Assets/MoveCharacter.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class MoveCharacter : MonoBehaviour {
 	public float speed;
+	public float speedUp;
 	private Rigidbody rb;
 
 	// Use this for initialization
@@ -14,9 +15,18 @@ public class MoveCharacter : MonoBehaviour {
 	void FixedUpdate () {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
+		float moveUp = Input.GetAxis ("Jump");
 
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-		rb.AddForce (movement);
+		Vector3 jump = new Vector3 (0.0f, moveUp, 0.0f);
+
+		rb.AddForce (movement * speed);
+		rb.AddForce (jump * speedUp);
 	}
 
+	void OnCollisionEnter (Collision other) {
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+		}
+	}
 }
