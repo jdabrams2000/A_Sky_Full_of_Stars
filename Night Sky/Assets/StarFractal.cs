c542877434e3f5e245c15d222525e11a0f839087
jdabrams2000 <abramsj@bxscience.edu>
2017-09-27 00:01:43 -0400

    Add Project #1

---
+++ Night Sky/Assets/StarFractal.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarFractal : MonoBehaviour {
+
+	public Mesh mesh;
+	public Material material;
+
+	public int maxDepth;
+	public int depth;
+
+	private void Start() {
+		gameObject.AddComponent<MeshFilter> ().mesh = mesh;
+		gameObject.AddComponent<MeshRenderer> ().material = material;
+		if (depth < maxDepth) {
+			new GameObject ("Fractal Child").AddComponent<StarFractal> ().Initialize (this);
+		}
+	}
+
+	private void Initialize (StarFractal parent) {
+		mesh = parent.mesh;
+		material = parent.material;
+		maxDepth = parent.maxDepth;
+		depth = parent.depth + 1;
+		transform.parent = parent.transform;
+	}
+
+}
